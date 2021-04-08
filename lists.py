ca0423f4838688441803ad3f1edd41ae6cb442e2
Conspiciens <fernandezjohann08@gmail.com>
2021-04-07 22:12:09 -0700

    Audio File and writes it into text

--- lists.py
+++ lists.py
@@ -1,5 +1,6 @@
 import wikipedia
 import speech_recognition as sr
 
+
 def important_commands(): 
     
--- main.py
+++ main.py
@@ -7,21 +7,21 @@ import speech_recognition as sr
 import wikipedia
 import json
 
-from lists import the_lists
-from lists import listening_command
-from lists import open_file_read
+# from lists import the_lists
+# from lists import listening_command
+# from lists import open_file_read
 
 r = sr.Recognizer()
 
 def input_sound_file(name_file): 
     recording = sr.AudioFile(name_file)
-    for reord in recording: 
-        audio = r.record(source)
+    with recording as record: 
+        audio = r.record(record)
 
-    r.recognize_google(audio)
+    print(r.recognize_google(audio))
 
 
 
 
 if __name__ == '__main__':
-    
+    input_sound_file("read.wav")
